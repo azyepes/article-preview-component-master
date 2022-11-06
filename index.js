837760e4c835e9593d12c07d8ea362fc53d57ec7
Luis Yps Fdez <lueryefe@gmail.com>
2022-11-06 16:48:34 -0500

    Hidding share section fix2

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ let shareButton = $('#share-button')
 let shareBox = $('#share-box')
 let profileBox = $('#profile-box')
 
-let bodyWidth = document.getElementsByTagName('body')[0].clientWidth
+
 
 shareButton.addEventListener('click', () => {
 
+    let bodyWidth = document.getElementsByTagName('body')[0].clientWidth
     if (bodyWidth >= 1440) {
         
         shareBox.classList.remove('inactive')
